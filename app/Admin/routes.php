93fcbe79c763d4c559e983dc9a7e300604ae6cde
zhangyulei <zhangyulei@localhost.localdomain>
2020-07-30 08:51:35 +0000

    后台-商品

---
+++ app/Admin/routes.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Routing\Router;
+
+Admin::routes();
+
+Route::group([
+    'prefix'        => config('admin.route.prefix'),
+    'namespace'     => config('admin.route.namespace'),
+    'middleware'    => config('admin.route.middleware'),
+    'as'            => config('admin.route.prefix') . '.',
+], function (Router $router) {
+
+    $router->get('/', 'HomeController@index')->name('home');
+    //商品管理
+    $router->resource('goods', GoodsController::class);
+});
